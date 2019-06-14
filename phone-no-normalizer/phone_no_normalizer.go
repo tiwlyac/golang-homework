4e1779c21cf0ee1fceccd2fa24fe2ec26f7e43c5
Anun Chaichomphoo <tiwlychaichomphoo@gmail.com>
2019-06-15 02:31:45 +0700

    implement phone number normalizer function

--- phone-no-normalizer/phone_no_normalizer.go
+++ phone-no-normalizer/phone_no_normalizer.go
@@ -1,9 +1,38 @@
 package main
 
-func phoneNoNormalizer(list []string) {
+import (
+	"fmt"
+	"regexp"
+)
 
+func phoneNoNormalizer(list []string) {
+	result := make(map[string]int)
+	regex, err := regexp.Compile("[^0-9]")
+	if err != nil {
+		fmt.Println("regex is incorrect")
+		return
+	}
+
+	for _, number := range list {
+		phoneNo := regex.ReplaceAllString(number, "")
+		result[phoneNo]++
+	}
+
+	for number, count := range result {
+		fmt.Printf("%v : %v\n", number, count)
+	}
 }
 
 func main() {
-
+	list := []string{
+		"1234567890",
+		"123 456 7891",
+		"123-456-7890",
+		"1234567892",
+		"(123) 456 7892",
+		"(123) 456-7893",
+		"123-456-7894",
+		"(123)456-7892",
+	}
+	phoneNoNormalizer(list)
 }
